b9f3c5f5284af294f1797b0175f0d9df4a22e879
Rafael <Rafael@Rafael-PC>
2019-07-22 06:36:00 -0500

    Tarea

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -1,11 +1,62 @@
-
 package practica;
+
+
+import java.util.Scanner;
 
 public class Practica {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
-        System.out.println("actulizando");
+        Scanner entrada = new Scanner(System.in);
+
+        int pregunta1;
+        String pregunta2;
+        int pregunta3;
+        String pregunta4;
+        int pregunta5;
+
+        System.out.println(" Ingrese un numero del escritorio ");
+        pregunta1 = entrada.nextInt();
+        if ("si ".equals(pregunta1)) {
+
+        }
+        System.out.println(" Que pida un usuario un documento ");
+        pregunta2 = entrada.next();
+        if (" si ".equals(pregunta2)) {
+
+        }
+        System.out.println(" Que pida imagenes ");
+        pregunta3 = entrada.nextInt();
+        if ("si".equals(pregunta3)) {
+
+        }
+
+        System.out.println(" Que pida  musica ");
+        pregunta4 = entrada.next();
+        if ("si ".equals(pregunta4)) {
+
+        }
+
+        System.out.println(" Que pida videos ");
+        pregunta5 = entrada.nextInt();
+
+        if ("si ".equals(pregunta5)) {
+          
+            
+            
+        }else{
+            
+            System.out.println("Por favor ingrese el numero de cual quiera que se la pregunta ");
+        
+         
+        }
     }
-    
 }
+                
+       
+    
+ 
+     
+
+         
+        
+
